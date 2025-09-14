672fa7cfcf7dc78a6ee7ea72f37c28a6e35a0588
keremustun444-cmyk <keremustun444@gmail.com>
2025-09-14 19:59:44 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,10 +1,11 @@
 module.exports = {
     // Botu Kullanmadan README.md dosyasını okuyun!
-    Token: "",
-    SahipID: "",
-    Prefix: "",
+    Token: "MTQxNjc1NjUzNzczMjYzMjYzNw.G6po8d.-z49VLW625uOZIsOLA5MaH5sNUNZs6gwgTW5iY",
+    SahipID: "1416745535192436857",
+    Prefix: "!",
 
     SahipAdı: "",
     ReklamSunucuLink: ""
     // Botu Kullanmadan README.md dosyasını okuyun!
 }
+
